395c2bcc94a35f9c13e5c8415579725105aa7270
jldaggs88 <jessalyn.daggs@gmail.com>
2020-12-09 00:14:14 -0600

    added stories and specfied data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Users from './components/content/Content.jsx';
+
+import Content from './components/content/Content.jsx';
 
 const App = () => {
 
@@ -9,14 +10,13 @@ const App = () => {
 
       <div className="jumbotron">
       <h1 className="display-4">Top Stories</h1>
-
       <p className="lead">Some text here</p>
-      <Users />
-      <hr className="my-4"/>
+      <Content />
+      {/* <hr className="my-4"/>
       <p>some boilerplate</p>
       <p className="lead">
         <a className="btn btn-primary btn-lg" href="#" role="button">See more</a>
-        </p>
+        </p> */}
 
       </div>
 
--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -2,17 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from '../../axios.js';
 
 const Content = () => {
-  const [topFive, setTopFive] = useState({});
+  const [topFive, setTopFive] = useState([]);
 
   const getData = () => {
     axios.get(`/analytics/posts`, {})
       .then(res => {
-        const data = res.data
+        const data = res.data.data;
         console.log(data);
-        // setTopFive(data);
-        // const content = data.map(e => {
-
-        // })
+        const top = data.map((e, i)=>{
+          // if(topFive === undefined){
+          //   return null;
+          // } else {
+            setTopFive([...topFive, topFive[i] = <div className="story-card">
+              <hr className="my-4"/>
+              <img src={e.image_url} alt="" /> 
+              <button class="story-link" onClick={(e) => {
+                e.preventDefault();
+                window.location.href=`${e.link}`;
+              }}>
+                {e.title}
+              </button>
+              <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">See more</a>
+              </p>
+            </div>])
+          // }
+        })
       })
       .catch((error) => {
         console.log(error)
@@ -21,10 +36,11 @@ const Content = () => {
 
   useEffect(() => {
     getData();
-  });
+  }, []);
 
   return (
-      <div>
+      <div className="stories-container">
+        {topFive}
       </div>
   )
 }
